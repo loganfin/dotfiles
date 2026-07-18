ee1515d7f6b26e36c16e700018585f468131071b
loganfin <finely4.logan@gmail.com>
2026-07-17 21:41:38 -0700

    Update treesitter and add rust-analyzer

---
+++ xdg_config/nvim/lua/config/lsp/rust_analyzer.lua
@@ -0,0 +1,2 @@
+--vim.lsp.config("rust-analyzer")
+vim.lsp.enable({ "rust_analyzer" })
--- xdg_config/nvim/lua/plugins/treesitter.lua
+++ xdg_config/nvim/lua/plugins/treesitter.lua
@@ -2,17 +2,55 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	dependencies = { "nvim-treesitter/nvim-treesitter-context", opts = {} },
 	-- Some config changes happened after this version which require reconfiguration.
-	tag = "v0.10.0",
+	--tag = "v0.10.0",
 	build = ":TSUpdate",
+	lazy = false,
 	config = function()
-		local configs = require("nvim-treesitter.configs")
+		local ts = require("nvim-treesitter")
+		ts.setup()
+		ts.install({
+			"c",
+			"cpp",
+			"lua",
+			"go",
+			"rust",
+		})
+	end,
+	init = function()
+		local pre_installed_parsers = {
+			"c",
+			"lua",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"vim",
+			"vimdoc",
+		}
 
-		configs.setup({
-			ensure_installed = { "c", "cpp", "lua", "go" },
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				local start_treesitter = function()
+					vim.treesitter.start()
+					vim.bo.indentexpr =
+						"v:lua.require'nvim-treesitter'.indentexpr()"
+				end
+
+				local ts = require("nvim-treesitter")
+				local lang = vim.treesitter.language.get_lang(args.match)
+				if vim.list_contains(ts.get_available(), lang) then
+					if
+						not vim.list_contains(ts.get_installed(), lang)
+						and not vim.list_contains(pre_installed_parsers, lang)
+					then
+						ts.install(lang):await(function()
+							start_treesitter()
+						end)
+					else
+						start_treesitter()
+					end
+				end
+			end,
+			desc = "Enable nvim-treesitter and install parser if not installed",
 		})
 	end,
 }
